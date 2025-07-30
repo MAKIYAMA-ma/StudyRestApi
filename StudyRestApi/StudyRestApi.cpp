de41b047c6816174131a81af54dbc32e3b54d7f7
MAKIYAMA <aaaywr29@yahoo.co.jp>
2025-07-30 13:17:36 +0900

    modification to access JSONPlaceholder

--- StudyRestApi/StudyRestApi.cpp
+++ StudyRestApi/StudyRestApi.cpp
@@ -1,18 +1,45 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 
-int main(void) {
-    CURL* curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.110/index.html");
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+#define BUF_SIZE 65536
 
-        CURLcode res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                curl_easy_strerror(res));
-        }
-        curl_easy_cleanup(curl);
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+  size_t totalSize = size * nmemb;
+  char* buffer = (char*)userp;
+  errno_t err = strncat_s(buffer, BUF_SIZE, (const char*)contents, totalSize);
+  if (err != 0) {
+    fprintf(stderr, "strncat_s failed: %d\n", err);
+  }
+  return totalSize;
+}
+
+int main(void) {
+  CURL* curl = curl_easy_init();
+  if (curl) {
+    char buffer[BUF_SIZE] = {0};
+
+    /* curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.110/index.html"); */
+    /* curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/"); */
+    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts");
+    curl_easy_setopt(curl, CURLOPT_CAINFO, "../../cert/cacert.pem");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, buffer);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
+
+    // 開発中のみ！ 本番ではNG！！
+    /* curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); */
+    /* curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); */
+
+    CURLcode res = curl_easy_perform(curl);
+    if (res == CURLE_OK) {
+      printf("Received:\n%s\n", buffer);
+    } else {
+      fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
     }
-    return 0;
+    curl_easy_cleanup(curl);
+  }
+  return 0;
 }
