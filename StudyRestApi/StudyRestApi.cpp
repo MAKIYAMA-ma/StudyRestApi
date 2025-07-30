281261aa4bc1308067fc4c034fc438a03d531afe
MAKIYAMA <aaaywr29@yahoo.co.jp>
2025-07-30 13:30:42 +0900

    modification to select endpoint

--- StudyRestApi/StudyRestApi.cpp
+++ StudyRestApi/StudyRestApi.cpp
@@ -5,8 +5,7 @@
 
 #define BUF_SIZE 65536
 
-static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-{
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
   size_t totalSize = size * nmemb;
   char* buffer = (char*)userp;
   errno_t err = strncat_s(buffer, BUF_SIZE, (const char*)contents, totalSize);
@@ -16,14 +15,24 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* use
   return totalSize;
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  char command[16] = "posts";
+  char url[64] = "https://jsonplaceholder.typicode.com/";
+
+  if(argc > 1) {
+    strncpy_s(command, sizeof(command), argv[1], sizeof(command));
+  }
+  printf("%s\n", command);
+  errno_t err = strncat_s(url, 64, command, 64);
+
   CURL* curl = curl_easy_init();
   if (curl) {
     char buffer[BUF_SIZE] = {0};
 
     /* curl_easy_setopt(curl, CURLOPT_URL, "http://192.168.1.110/index.html"); */
     /* curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com/"); */
-    curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts");
+    /* curl_easy_setopt(curl, CURLOPT_URL, "https://jsonplaceholder.typicode.com/posts"); */
+    curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_CAINFO, "../../cert/cacert.pem");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, buffer);
